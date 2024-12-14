02217824c2ac23eb74da9f41fa94ee15325e830a
Ted Senft <code@robotgryphon.com>
2024-12-13 20:21:28 -0500

    Version?

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,3 @@
-val isRelease: Boolean = (System.getenv("RELEASE") ?: "false").equals("true", true)
-
 plugins {
     id("gander-convention")
 }
--- levels/build.gradle.kts
+++ levels/build.gradle.kts
@@ -1,12 +1,3 @@
 plugins {
     id("gander-convention")
 }
-
-// simply exists to apply the module convention script
-// sets up MDG workspace and includes manifest data from resources dir
-// additional setup specific to this module may be applied here
-
-//mixin {
-//    add(sourceSets[SourceSet.MAIN_SOURCE_SET_NAME], "gander_levels.mixins.json")
-//    config("gander_levels.mixins.json")
-//}
